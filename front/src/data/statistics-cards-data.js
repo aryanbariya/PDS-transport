603a00bd77bf04d67bb43c6374728dd6c9fe5b51
Harsh Marvaniya <harshmarvaniya.4@gmail.com>
2025-02-19 18:52:22 +0530

    old data recover

--- front/src/data/statistics-cards-data.js
+++ front/src/data/statistics-cards-data.js
@@ -1,11 +1,80 @@
+// import {
+//   BanknotesIcon,
+//   UserPlusIcon,
+//   UsersIcon,
+//   ChartBarIcon,
+//   TruckIcon,
+//   BuildingStorefrontIcon,
+//   // PackageIcon,
+// } from "@heroicons/react/24/solid";
+
+// export const statisticsCardsData = [
+//   {
+//     color: "gray",
+//     icon: BanknotesIcon,
+//     title: "OWNERS",
+//     value: "$53k",
+//     route: "/OwnerNamePage", 
+//   },
+//   {
+//     color: "gray",
+//     icon: UsersIcon,
+//     title: "EMPLOYEES",
+//     value: "2,300",
+//     route: "/EmployeePage",
+//   },
+//   {
+//     color: "gray",
+//     icon: UserPlusIcon,
+//     title: "MSWC",
+//     value: "3,462",
+//     route: "/MSWCGodownPage",
+//   },
+//   {
+//     color: "gray",
+//     icon: ChartBarIcon,
+//     title: "SubGodown",
+//     value: "15 Warehouses",
+//     route: "/SubGodownPage",
+//   },
+//   {
+//     color: "gray",
+//     icon: TruckIcon,
+//     title: "CATEGORY",
+//     value: "120",
+//     route: "/CategoryPage",
+//   },
+//   {
+//     color: "gray",
+//     icon: TruckIcon,
+//     title: "TRUCKS",
+//     value: "85",
+//     route: "/TruckPage",
+//   },
+//   {
+//     color: "gray",
+//     icon: BuildingStorefrontIcon,
+//     title: "SCHEMES",
+//     value: "12 Active",
+//     route: "/SchemePage",
+//   },
+//   {
+//     color: "gray",
+//     icon: BuildingStorefrontIcon,
+//     title: "PACKAGING",
+//     value: "40k Units",
+//     route: "/PackagingPage",
+//   },
+// ];
+
+// export default statisticsCardsData;
+
+
 import {
   BanknotesIcon,
   UserPlusIcon,
   UsersIcon,
   ChartBarIcon,
-  TruckIcon,
-  BuildingStorefrontIcon,
-  // PackageIcon,
 } from "@heroicons/react/24/solid";
 
 export const statisticsCardsData = [
@@ -14,56 +83,88 @@ export const statisticsCardsData = [
     icon: BanknotesIcon,
     title: "OWNERS",
     value: "$53k",
-    route: "/OwnerNamePage", 
+    footer: {
+      color: "text-green-500",
+      value: "+55%",
+      label: "than last week",
+    },
   },
   {
     color: "gray",
     icon: UsersIcon,
     title: "EMPLOYEES",
     value: "2,300",
-    route: "/EmployeePage",
+    footer: {
+      color: "text-green-500",
+      value: "+3%",
+      label: "than last month",
+    },
   },
   {
     color: "gray",
     icon: UserPlusIcon,
     title: "MSWC",
     value: "3,462",
-    route: "/MSWCGodownPage",
+    footer: {
+      color: "text-red-500",
+      value: "-2%",
+      label: "than yesterday",
+    },
   },
   {
     color: "gray",
     icon: ChartBarIcon,
-    title: "SubGodown",
-    value: "15 Warehouses",
-    route: "/SubGodownPage",
+    title: "GODOWN",
+    value: "$103,430",
+    footer: {
+      color: "text-green-500",
+      value: "+5%",
+      label: "than yesterday",
+    },
   },
   {
     color: "gray",
-    icon: TruckIcon,
-    title: "CATEGORY",
-    value: "120",
-    route: "/CategoryPage",
+    icon: ChartBarIcon,
+    title: "DRIVERS",
+    value: "$103,430",
+    footer: {
+      color: "text-green-500",
+      value: "+5%",
+      label: "than yesterday",
+    },
   },
   {
     color: "gray",
-    icon: TruckIcon,
+    icon: ChartBarIcon,
     title: "TRUCKS",
-    value: "85",
-    route: "/TruckPage",
+    value: "$103,430",
+    footer: {
+      color: "text-green-500",
+      value: "+5%",
+      label: "than yesterday",
+    },
   },
   {
     color: "gray",
-    icon: BuildingStorefrontIcon,
+    icon: ChartBarIcon,
     title: "SCHEMES",
-    value: "12 Active",
-    route: "/SchemePage",
+    value: "$103,430",
+    footer: {
+      color: "text-green-500",
+      value: "+5%",
+      label: "than yesterday",
+    },
   },
   {
     color: "gray",
-    icon: BuildingStorefrontIcon,
+    icon: ChartBarIcon,
     title: "PACKAGING",
-    value: "40k Units",
-    route: "/PackagingPage",
+    value: "$103,430",
+    footer: {
+      color: "text-green-500",
+      value: "+5%",
+      label: "than yesterday",
+    },
   },
 ];
 
