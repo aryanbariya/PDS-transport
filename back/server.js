1b2b29ce100ff81045861794fcf6087007c79e75
aryanbariya <aryanbariya777@gmail.com>
2025-02-25 11:15:07 +0530

    trying deployment

--- back/server.js
+++ back/server.js
@@ -19,7 +19,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASS || "",
   database: process.env.DB_NAME || "pdsmanagement",
-  // port: process.env.DB_PORT
+  port: process.env.DB_PORT || 3306, 
 });
 
 db.connect((err) => {
