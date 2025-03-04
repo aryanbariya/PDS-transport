2b66ee95153840cc78dce31fa18111dc730a6d1d
Harsh Marvaniya <harshmarvaniya.4@gmail.com>
2025-03-04 15:40:15 +0530

    commit

--- front/src/pages/auth/sign-in.jsx
+++ front/src/pages/auth/sign-in.jsx
@@ -1,3 +1,172 @@
+// import {
+//   Card,
+//   Input,
+//   Checkbox,
+//   Button,
+//   Typography,
+// } from "@material-tailwind/react";
+// import { Link, useNavigate } from "react-router-dom";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import { toast, ToastContainer } from "react-toastify";
+
+
+// const URL = import.meta.env.VITE_API_BACK_URL
+
+// export function SignIn() {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const navigate = useNavigate(); // Redirect after login
+//   const [errors, setErrors] = useState({});
+
+
+//   const handleSignIn = async (e) => {
+//     e.preventDefault();
+
+//     if (!email || !password) {
+//       alert("Please enter email and password");
+//       setErrors({ allFields: "All fields are required" });
+//       return;
+//     }
+
+
+//     try {
+//       const response = await axios.post(`${URL}/signin`, { email, password });
+
+//       if (response.status === 200) {
+//         localStorage.setItem("token", response.data.token); // Store token
+//         alert("Login Successful!");
+//         navigate("/dashboard"); // Redirect to dashboard (update path if needed)
+//         ;
+//       }
+//     } catch (err) {
+//       alert(err.response?.data?.error || "Login failed. Please try again.");
+//     }
+
+
+//   };
+//   return (
+//     <section className="m-8 flex gap-4">
+//       <div className="w-full lg:w-3/5 mt-24">
+//         <div className="text-center">
+//           <Typography variant="h2" className="font-bold mb-4">Sign In</Typography>
+//           <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal">Enter your email and password to Sign In.</Typography>
+//         </div>
+//         <form onSubmit={handleSignIn} className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2">
+//           {errors.allFields && (<Typography variant="small" color="red" className="text-center">{errors.allFields}</Typography>)}
+//           <div className="mb-1 flex flex-col gap-6">
+//             <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
+//               Your email
+//             </Typography>
+//             <Input
+//               size="lg"
+//               placeholder="name@mail.com"
+//               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+//               value={email}
+//               labelProps={{
+//                 className: "before:content-none after:content-none",
+//               }}
+//               onChange={(e) => setEmail(e.target.value)}
+//             />
+//             <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
+//               Password
+//             </Typography>
+//             <Input
+//               type="password"
+//               size="lg"
+//               placeholder="********"
+//               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+//               labelProps={{
+//                 className: "before:content-none after:content-none",
+//               }}
+//               onChange={(e) => setPassword(e.target.value)}
+//             />
+//           </div>
+//           <Checkbox
+//             label={
+//               <Typography
+//                 variant="small"
+//                 color="gray"
+//                 className="flex items-center justify-start font-medium"
+//               >
+//                 I agree the&nbsp;
+//                 <a
+//                   href="#"
+//                   className="font-normal text-black transition-colors hover:text-gray-900 underline"
+//                 >
+//                   Terms and Conditions
+//                 </a>
+//               </Typography>
+//             }
+//             containerProps={{ className: "-ml-2.5" }}
+//           />
+//           <Button className="mt-6" fullWidth type="submit">
+//             Sign In
+//           </Button>
+
+//           <div className="flex items-center justify-between gap-2 mt-6">
+//             <Checkbox
+//               label={
+//                 <Typography
+//                   variant="small"
+//                   color="gray"
+//                   className="flex items-center justify-start font-medium"
+//                 >
+//                   Subscribe me to newsletter
+//                 </Typography>
+//               }
+//               containerProps={{ className: "-ml-2.5" }}
+//             />
+//             <Typography variant="small" className="font-medium text-gray-900">
+//               <a href="#">
+//                 Forgot Password
+//               </a>
+//             </Typography>
+//           </div>
+//           <div className="space-y-4 mt-8">
+//             <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth>
+//               <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+//                 <g clipPath="url(#clip0_1156_824)">
+//                   <path d="M16.3442 8.18429C16.3442 7.64047 16.3001 7.09371 16.206 6.55872H8.66016V9.63937H12.9813C12.802 10.6329 12.2258 11.5119 11.3822 12.0704V14.0693H13.9602C15.4741 12.6759 16.3442 10.6182 16.3442 8.18429Z" fill="#4285F4" />
+//                   <path d="M8.65974 16.0006C10.8174 16.0006 12.637 15.2922 13.9627 14.0693L11.3847 12.0704C10.6675 12.5584 9.7415 12.8347 8.66268 12.8347C6.5756 12.8347 4.80598 11.4266 4.17104 9.53357H1.51074V11.5942C2.86882 14.2956 5.63494 16.0006 8.65974 16.0006Z" fill="#34A853" />
+//                   <path d="M4.16852 9.53356C3.83341 8.53999 3.83341 7.46411 4.16852 6.47054V4.40991H1.51116C0.376489 6.67043 0.376489 9.33367 1.51116 11.5942L4.16852 9.53356Z" fill="#FBBC04" />
+//                   <path d="M8.65974 3.16644C9.80029 3.1488 10.9026 3.57798 11.7286 4.36578L14.0127 2.08174C12.5664 0.72367 10.6469 -0.0229773 8.65974 0.000539111C5.63494 0.000539111 2.86882 1.70548 1.51074 4.40987L4.1681 6.4705C4.8001 4.57449 6.57266 3.16644 8.65974 3.16644Z" fill="#EA4335" />
+//                 </g>
+//                 <defs>
+//                   <clipPath id="clip0_1156_824">
+//                     <rect width="16" height="16" fill="white" transform="translate(0.5)" />
+//                   </clipPath>
+//                 </defs>
+//               </svg>
+//               <span>Sign in With Google</span>
+//             </Button>
+//             <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth>
+//               <img src="/img/twitter-logo.svg" height={24} width={24} alt="" />
+//               <span>Sign in With Twitter</span>
+//             </Button>
+//           </div>
+//           <Typography variant="paragraph" className="text-center text-blue-gray-500 font-medium mt-4">
+//             Not registered?
+//             <Link to="/auth/sign-up" className="text-gray-900 ml-1">Create account</Link>
+//           </Typography>
+//         </form>
+
+//       </div>
+//       <div className="w-2/5 h-full hidden lg:block">
+//         <img
+//           src="/img/pattern.png"
+//           className="h-full w-full object-cover rounded-3xl"
+//         />
+//       </div>
+
+//     </section>
+//   );
+// }
+
+// export default SignIn;
+
+
+
 import {
   Card,
   Input,
@@ -6,162 +175,94 @@ import {
   Typography,
 } from "@material-tailwind/react";
 import { Link, useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { toast, ToastContainer } from "react-toastify";
 
-
-const URL = import.meta.env.VITE_API_BACK_URL
+const URL = import.meta.env.VITE_API_BACK_URL;
 
 export function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate(); // Redirect after login
+  const navigate = useNavigate();
   const [errors, setErrors] = useState({});
-
 
   const handleSignIn = async (e) => {
     e.preventDefault();
 
     if (!email || !password) {
-      alert("Please enter email and password");
+      toast.error("Please enter email and password");
       setErrors({ allFields: "All fields are required" });
       return;
     }
 
-
     try {
       const response = await axios.post(`${URL}/signin`, { email, password });
-
       if (response.status === 200) {
-        localStorage.setItem("token", response.data.token); // Store token
-        alert("Login Successful!");
-        navigate("/dashboard"); // Redirect to dashboard (update path if needed)
-        ;
+        localStorage.setItem("token", response.data.token);
+        toast.success("Login Successful!");
+        navigate("/dashboard");
       }
     } catch (err) {
-      alert(err.response?.data?.error || "Login failed. Please try again.");
+      toast.error(err.response?.data?.error || "Login failed. Please try again.");
     }
-
-
   };
+
   return (
-    <section className="m-8 flex gap-4">
-      <div className="w-full lg:w-3/5 mt-24">
-        <div className="text-center">
-          <Typography variant="h2" className="font-bold mb-4">Sign In</Typography>
-          <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal">Enter your email and password to Sign In.</Typography>
-        </div>
-        <form onSubmit={handleSignIn} className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2">
-          {errors.allFields && (<Typography variant="small" color="red" className="text-center">{errors.allFields}</Typography>)}
-          <div className="mb-1 flex flex-col gap-6">
-            <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
-              Your email
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-700 to-blue-900">
+      <Card className="w-full max-w-md p-8 shadow-lg rounded-lg bg-white">
+        <Typography variant="h3" className="text-center text-blue-800 font-bold">
+          Welcome Back!
+        </Typography>
+        <Typography className="text-center text-gray-600 mb-6">
+          Sign in to continue to your PDS dashboard.
+        </Typography>
+        <form onSubmit={handleSignIn}>
+          {errors.allFields && (
+            <Typography variant="small" color="red" className="text-center">
+              {errors.allFields}
             </Typography>
+          )}
+          <div className="mb-4">
+            <Typography className="font-medium">Email</Typography>
             <Input
-              size="lg"
-              placeholder="name@mail.com"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              type="email"
+              placeholder="Enter your email"
               value={email}
-              labelProps={{
-                className: "before:content-none after:content-none",
-              }}
+              className="border border-gray-300 rounded-lg p-3 w-full"
               onChange={(e) => setEmail(e.target.value)}
             />
-            <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
-              Password
-            </Typography>
+          </div>
+          <div className="mb-4">
+            <Typography className="font-medium">Password</Typography>
             <Input
               type="password"
-              size="lg"
-              placeholder="********"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-              labelProps={{
-                className: "before:content-none after:content-none",
-              }}
+              placeholder="Enter your password"
+              value={password}
+              className="border border-gray-300 rounded-lg p-3 w-full"
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <Checkbox
-            label={
-              <Typography
-                variant="small"
-                color="gray"
-                className="flex items-center justify-start font-medium"
-              >
-                I agree the&nbsp;
-                <a
-                  href="#"
-                  className="font-normal text-black transition-colors hover:text-gray-900 underline"
-                >
-                  Terms and Conditions
-                </a>
-              </Typography>
-            }
-            containerProps={{ className: "-ml-2.5" }}
-          />
-          <Button className="mt-6" fullWidth type="submit">
-            Sign In
-          </Button>
-
-          <div className="flex items-center justify-between gap-2 mt-6">
-            <Checkbox
-              label={
-                <Typography
-                  variant="small"
-                  color="gray"
-                  className="flex items-center justify-start font-medium"
-                >
-                  Subscribe me to newsletter
-                </Typography>
-              }
-              containerProps={{ className: "-ml-2.5" }}
-            />
-            <Typography variant="small" className="font-medium text-gray-900">
-              <a href="#">
-                Forgot Password
-              </a>
+          <div className="flex items-center justify-between mb-6">
+            <Checkbox label={<Typography>Remember me</Typography>} />
+            <Typography className="text-blue-700 cursor-pointer hover:underline">
+              Forgot Password?
             </Typography>
           </div>
-          <div className="space-y-4 mt-8">
-            <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth>
-              <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clipPath="url(#clip0_1156_824)">
-                  <path d="M16.3442 8.18429C16.3442 7.64047 16.3001 7.09371 16.206 6.55872H8.66016V9.63937H12.9813C12.802 10.6329 12.2258 11.5119 11.3822 12.0704V14.0693H13.9602C15.4741 12.6759 16.3442 10.6182 16.3442 8.18429Z" fill="#4285F4" />
-                  <path d="M8.65974 16.0006C10.8174 16.0006 12.637 15.2922 13.9627 14.0693L11.3847 12.0704C10.6675 12.5584 9.7415 12.8347 8.66268 12.8347C6.5756 12.8347 4.80598 11.4266 4.17104 9.53357H1.51074V11.5942C2.86882 14.2956 5.63494 16.0006 8.65974 16.0006Z" fill="#34A853" />
-                  <path d="M4.16852 9.53356C3.83341 8.53999 3.83341 7.46411 4.16852 6.47054V4.40991H1.51116C0.376489 6.67043 0.376489 9.33367 1.51116 11.5942L4.16852 9.53356Z" fill="#FBBC04" />
-                  <path d="M8.65974 3.16644C9.80029 3.1488 10.9026 3.57798 11.7286 4.36578L14.0127 2.08174C12.5664 0.72367 10.6469 -0.0229773 8.65974 0.000539111C5.63494 0.000539111 2.86882 1.70548 1.51074 4.40987L4.1681 6.4705C4.8001 4.57449 6.57266 3.16644 8.65974 3.16644Z" fill="#EA4335" />
-                </g>
-                <defs>
-                  <clipPath id="clip0_1156_824">
-                    <rect width="16" height="16" fill="white" transform="translate(0.5)" />
-                  </clipPath>
-                </defs>
-              </svg>
-              <span>Sign in With Google</span>
-            </Button>
-            <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth>
-              <img src="/img/twitter-logo.svg" height={24} width={24} alt="" />
-              <span>Sign in With Twitter</span>
-            </Button>
-          </div>
-          <Typography variant="paragraph" className="text-center text-blue-gray-500 font-medium mt-4">
-            Not registered?
-            <Link to="/auth/sign-up" className="text-gray-900 ml-1">Create account</Link>
+          <Button type="submit" className="w-full bg-blue-700 hover:bg-blue-800 text-white py-2 rounded-lg">
+            Sign In
+          </Button>
+          <Typography className="text-center text-gray-600 mt-4">
+            Don't have an account?
+            <Link to="/auth/sign-up" className="text-blue-700 ml-1 hover:underline">
+              Create one
+            </Link>
           </Typography>
         </form>
-
-      </div>
-      <div className="w-2/5 h-full hidden lg:block">
-        <img
-          src="/img/pattern.png"
-          className="h-full w-full object-cover rounded-3xl"
-        />
-      </div>
-
-    </section>
+      </Card>
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar closeOnClick pauseOnHover draggable />
+    </div>
   );
 }
 
 export default SignIn;
-
--- front/src/widgets/layout/footer.jsx
+++ front/src/widgets/layout/footer.jsx
@@ -1,34 +1,106 @@
+// import PropTypes from "prop-types";
+// import { Typography } from "@material-tailwind/react";
+// import { HeartIcon } from "@heroicons/react/24/solid";
+
+// export function Footer({ brandName, brandLink, routes }) {
+//   const year = new Date().getFullYear();
+
+//   return (
+//     <footer className="py-2">
+//       <div className="flex w-full flex-wrap items-center justify-center gap-6 px-2 md:justify-between">
+//         <Typography variant="small" className="font-normal text-inherit">
+//           &copy; {year}, made with{" "}
+//           <HeartIcon className="-mt-0.5 inline-block h-3.5 w-3.5 text-red-600" /> by{" "}
+//           <a
+//             href={brandLink}
+//             target="_blank"
+//             className="transition-colors hover:text-blue-500 font-bold"
+//           >
+//             {brandName}
+//           </a>{" "}
+//           for a better web.
+//         </Typography>
+//         <ul className="flex items-center gap-4">
+//           {routes.map(({ name, path }) => (
+//             <li key={name}>
+//               <Typography
+//                 as="a"
+//                 href={path}
+//                 target="_blank"
+//                 variant="small"
+//                 className="py-0.5 px-1 font-normal text-inherit transition-colors hover:text-blue-500"
+//               >
+//                 {name}
+//               </Typography>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </footer>
+//   );
+// }
+
+// Footer.defaultProps = {
+//   brandName: "Creative Aryan",
+//   brandLink: "https://www.creative-tim.com",
+//   routes: [
+//     { name: "Creative Tim",  },
+//     { name: "About Us",  },
+//     { name: "Blog",  },
+//     { name: "License", },
+//     // { name: "Creative Tim", path: "https://www.creative-tim.com" },
+//     // { name: "About Us", path: "https://www.creative-tim.com/presentation" },
+//     // { name: "Blog", path: "https://www.creative-tim.com/blog" },
+//     // { name: "License", path: "https://www.creative-tim.com/license" },
+//   ],
+// };
+
+// Footer.propTypes = {
+//   brandName: PropTypes.string,
+//   brandLink: PropTypes.string,
+//   routes: PropTypes.arrayOf(PropTypes.object),
+// };
+
+// Footer.displayName = "/src/widgets/layout/footer.jsx";
+
+// export default Footer;
+
+
+
 import PropTypes from "prop-types";
 import { Typography } from "@material-tailwind/react";
 import { HeartIcon } from "@heroicons/react/24/solid";
 
-export function Footer({ brandName, brandLink, routes }) {
+export function Footer({ isSidebarOpen }) {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="py-2">
-      <div className="flex w-full flex-wrap items-center justify-center gap-6 px-2 md:justify-between">
-        <Typography variant="small" className="font-normal text-inherit">
+    <footer className="fixed bottom-4 left-4 right-4 transition-all duration-300">
+      <div
+        className={`w-full max-w-[calc(100%-2rem)] bg-white py-4 px-6 flex flex-wrap items-center justify-center md:justify-between shadow-lg rounded-2xl
+        ${isSidebarOpen ? "md:ml-[260px] md:w-[calc(100%-280px)]" : "md:ml-[80px] md:w-[calc(100%-100px)]"}`}
+      >
+        <Typography variant="small" className="font-normal text-gray-700 text-center">
           &copy; {year}, made with{" "}
           <HeartIcon className="-mt-0.5 inline-block h-3.5 w-3.5 text-red-600" /> by{" "}
           <a
-            href={brandLink}
+            href="https://www.creative-tim.com"
             target="_blank"
+            rel="noopener noreferrer"
             className="transition-colors hover:text-blue-500 font-bold"
           >
-            {brandName}
+            Creative Aryan
           </a>{" "}
           for a better web.
         </Typography>
         <ul className="flex items-center gap-4">
-          {routes.map(({ name, path }) => (
-            <li key={name}>
+          {["About Us", "Blog", "License"].map((name, index) => (
+            <li key={index}>
               <Typography
                 as="a"
-                href={path}
-                target="_blank"
+                href="#"
                 variant="small"
-                className="py-0.5 px-1 font-normal text-inherit transition-colors hover:text-blue-500"
+                className="py-0.5 px-1 font-normal text-gray-700 transition-colors hover:text-blue-500"
               >
                 {name}
               </Typography>
@@ -40,27 +112,12 @@ export function Footer({ brandName, brandLink, routes }) {
   );
 }
 
-Footer.defaultProps = {
-  brandName: "Creative Aryan",
-  brandLink: "https://www.creative-tim.com",
-  routes: [
-    { name: "Creative Tim",  },
-    { name: "About Us",  },
-    { name: "Blog",  },
-    { name: "License", },
-    // { name: "Creative Tim", path: "https://www.creative-tim.com" },
-    // { name: "About Us", path: "https://www.creative-tim.com/presentation" },
-    // { name: "Blog", path: "https://www.creative-tim.com/blog" },
-    // { name: "License", path: "https://www.creative-tim.com/license" },
-  ],
-};
-
 Footer.propTypes = {
-  brandName: PropTypes.string,
-  brandLink: PropTypes.string,
-  routes: PropTypes.arrayOf(PropTypes.object),
+  isSidebarOpen: PropTypes.bool,
 };
 
-Footer.displayName = "/src/widgets/layout/footer.jsx";
+Footer.defaultProps = {
+  isSidebarOpen: true,
+};
 
 export default Footer;
