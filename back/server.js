025fc0bd52e4a43cd8da3c2f7c3df8592d2fda04
aryanbariya <aryanbariya777@gmail.com>
2025-04-09 11:05:19 +0530

    tring put do

--- back/server.js
+++ back/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require("mysql2/promise");
+const mysql = require("mysql");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
